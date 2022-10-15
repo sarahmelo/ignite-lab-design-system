a98bc052e2f5774ac75681edbc6031a40b234afc
sarahmelo <sarahcbdg@gmail.com>
2022-10-15 16:52:56 -0300

    finish project

---
+++ src/pages/Signin.stories.tsx
@@ -0,0 +1,40 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Signin } from './Signin'
+import { within, userEvent, waitFor } from '@storybook/testing-library'
+import { expect } from '@storybook/jest'
+import { rest } from 'msw';
+
+export default {
+        title: 'Components/Sign in',
+        component: Signin,
+        args: {
+                children: 'Button Component',
+        },
+        argTypes: { },
+        parameters: {
+                msw: {
+                        handlers: [
+                                rest.post('/sessions', (req, res, ctx) => {
+                                        return res(ctx.json({
+                                                message: "Login Realizado"
+                                        }))
+                                })
+                        ]
+                }
+        }
+} as Meta
+
+export const Default: StoryObj = {
+        play: async ({ canvasElement }) => {
+                const canvas = within(canvasElement)
+
+                userEvent.type(canvas.getByPlaceholderText('Digite seu e-mail'), 'sarah@mailiantor.com')
+                userEvent.type(canvas.getByLabelText('Sua senha'), 'pastelzinho')
+
+                userEvent.click(canvas.getByRole('button'))
+
+                await waitFor(() => {
+                        expect(canvas.getByText('Login Realizado')).toBeInTheDocument()
+                })
+        }
+}
